7007fb79348c773fdb06df318adf37a2688c9766
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-09-29 15:14:45 +0800

    Changed connection string

--- Session3FinalModel.Designer.cs
+++ Session3FinalModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Users\Dylan Koh\Source\Repos\Kazan_Session3_API_16_9\Session3Model.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Users\Dylan Koh\Source\Repos\Kazan_Session3_API_16_9\Session3FinalModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
